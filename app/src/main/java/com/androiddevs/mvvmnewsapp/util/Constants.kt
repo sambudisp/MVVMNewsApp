6ac61312d0fcbbcfe2d12b4a95986b20313fc566
Sambudi Sugya Putra <sambudisp@gmail.com>
2020-06-11 13:57:15 +0700

    Init project | setup api | setup navigation | setup response |

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants  {
+    companion object{
+        const val API_KEY = "01a1b636189d4741b31c4e67bf58ce8f"
+        const val BASE_URL = " https://newsapi.org"
+    }
+}
