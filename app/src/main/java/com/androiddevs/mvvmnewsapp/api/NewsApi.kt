65b9c579da36d956cf1f155d05152e7ba2ec2b68
Sambudi Sugya Putra <sambudisp@gmail.com>
2020-06-18 14:01:00 +0700

    4 - 9

--- app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsApi.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsApi.kt
@@ -1,6 +1,6 @@
 package com.androiddevs.mvvmnewsapp.api
 
-import com.androiddevs.mvvmnewsapp.NewsReponse
+import com.androiddevs.mvvmnewsapp.models.NewsReponse
 import com.androiddevs.mvvmnewsapp.util.Constants.Companion.API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants  {
     companion object{
         const val API_KEY = "01a1b636189d4741b31c4e67bf58ce8f"
         const val BASE_URL = " https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
